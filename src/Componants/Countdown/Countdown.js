da94d4b47e5b2359657faf122d518a357fdd5a87
Abdullah Sheikh <damladaxmad@gmail.com>
2021-10-21 01:36:02 -0700

    create countdown

--- src/Componants/Countdown/Countdown.js
+++ src/Componants/Countdown/Countdown.js
@@ -1,9 +1,38 @@
-import React from "react"
+import React from "react";
+import { Button } from "@mui/material";
 
-const Countdown = () =>{
-    return <div>
-     Countdown
+const Countdown = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#DD6662",
+        width: "35%",
+        margin: "40px auto",
+        padding: "20px",
+        borderRadius: "8px"
+      }}
+    >
+      <div style={{ display: "flex", justifyContent: "center", gap: "10px",
+    color: "white" }}>
+        <div style = {{backgroundColor: "#BC5753",
+    padding: "5px 10px", borderRadius: "8px",
+    fontWeight: "bold"}}> Pomodoro </div> <div 
+    style = {{backgroundColor: "",
+    padding: "5px 10px", borderRadius: "8px"}}> Short Break </div> <div
+    style = {{backgroundColor: "",
+    padding: "5px 10px", borderRadius: "8px"}}> Long Break </div>
+      </div>
+      <p style = {{ fontSize: "120px", fontWeight: "bolder",
+    padding: "0px", margin: "0px", color: "white"}}>25:00</p>
+      <Button variant="contained" style = {{
+          width: "200px", backgroundColor: "#FFFFFF",
+          color: "#D95550", padding: "8px", fontWeight: "bold",
+          fontSize: "24px", borderBottom: "5px solid #EBEBEB"
+      }}>
+        Start
+      </Button>
     </div>
-}
+  );
+};
 
 export default Countdown;
